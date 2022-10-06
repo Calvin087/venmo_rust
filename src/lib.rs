0670d2f8b0ef5abe4ed71d4d2dab91d3b885ecc2
Calvin <calvin@Calvins-MacBook-Air.local>
2022-10-06 08:26:51 +0200

    save

--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,27 @@ use near_sdk::{env, near_bindgen, setup_alloc, AccountId, Promise};
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Welcome {
-    memo: LookupMap<String, Vec<String>>,
+    memo: LookupMap<String, Vec<String>>
+
 }
 
 impl Default for Welcome {
     fn default() -> Self {
         Self {
-            memo: LookupMap::new(b"memo".to_vec()), /*
-                                                        When declaring b before the string, we're
-                                                        literally sending bytes to the blockchain.
-                                                        memo.to_vec === [109, 101, 109, 111]
-                                                    */
+            memo: LookupMap::new(b"memo".to_vec())
+            /* 
+                When declaring b before the string, we're
+                literally sending bytes to the blockchain.
+                memo.to_vec === [109, 101, 109, 111]
+            */
         }
     }
 }
 
 #[near_bindgen]
 impl Welcome {
-    /*
+
+    /* 
         Change Methods
     */
     pub fn add_memo(&mut self, memo_text: String, price: String) {
@@ -36,35 +39,38 @@ impl Welcome {
         // returns bool
 
         if contains_user {
-            let mut temp_list = match self.memo.get(&account_id) {
+            let mut temp_list = match self.memo.get(&account_id){
                 Some(x) => x, // x is the [] of memos owned by account_id
-                None => vec![],
+                None => vec![]
             };
 
-            temp_list.push(memo_text + " || " + &price + "NEAR");
+            temp_list.push(memo_text + " || " + &price+"NEAR");
             // requires an owned string on the left for concatenation
 
             self.memo.insert(&account_id, &temp_list);
         } else {
-            let initiate_vector = vec![memo_text + " || " + &price + "NEAR"];
+            let initiate_vector = vec![memo_text + " || " + &price+"NEAR"];
             self.memo.insert(&account_id, &initiate_vector);
         }
+
     }
 
     pub fn transfer_money(&mut self, account_id: AccountId, amount: f64) {
         Promise::new(account_id).transfer(amount as u128);
         // Sending money as a promise.
+
     }
 
-    /*
+    /* 
         View methods.
         No need to &mut self
     */
-    pub fn get_memos(self, user: String) -> Vec<String> {
+    pub fn get_memos(self, user:String) -> Vec<String> {
         match self.memo.get(&user) {
-            Some(x) => x,   // vec of memos
-            None => vec![], // empty vec
+            Some(x) => x, // vec of memos 
+            None => vec![] // empty vec
         }
     }
+
 }
 // https://youtu.be/zy5VgigLy-Y?t=978
